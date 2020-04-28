dd25ee55351f807c3a413b1bd7001536ddefed4e
cafloivedu <cafloivedu@outlook.com>
2020-04-28 18:17:45 -0500

    minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 
 import 'models/auth.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
-
 
 void main() => runApp(
       ChangeNotifierProvider<Auth>(
