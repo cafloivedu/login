71c498bcac6213de27a72a39018d676953608d00
Hernan Parra <hernandre1122@gmail.com>
2020-05-19 16:34:14 -0500

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,20 +18,18 @@ class _HomeState extends State<Home> {
   String email;
   Future<List> futureList;
   List<CourseInfo> _courses = [];
-  String profilePicture =
-      "https://api.adorable.io/avatars/285/${UserInfo().email}";
+  String profilePicture = "https://api.adorable.io/avatars/285/${Random()}";
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       drawer: new Drawer(
           child: new ListView(children: <Widget>[
         new UserAccountsDrawerHeader(
-            accountName: new Text("${_readPreferences()}"),
-            accountEmail: new Text("${UserInfo().email}"),
+            accountName: new Text("John Doe"),
+            accountEmail: new Text("random@example.com"),
             currentAccountPicture: new GestureDetector(
-                onTap: () => print(UserInfo().name),
+                onTap: () => print(" selected user info "),
                 child: new CircleAvatar(
                   backgroundImage: new NetworkImage(profilePicture),
                 )),
@@ -42,15 +40,11 @@ class _HomeState extends State<Home> {
                       "https://img00.deviantart.net/35f0/i/2015/018/2/6/low_poly_landscape__the_river_cut_by_bv_designs-d8eib00.jpg")),
             )),
         new ListTile(
-          title: new Text("Cursos"),
+          title: new Text("First Page"),
           trailing: new Icon(Icons.arrow_upward),
         ),
         new ListTile(
-          title: new Text("Profesores"),
-          trailing: new Icon(Icons.arrow_right),
-        ),
-        new ListTile(
-          title: new Text("Estudiantes"),
+          title: new Text("Second Page"),
           trailing: new Icon(Icons.arrow_right),
         ),
         new Divider(),
@@ -71,20 +65,18 @@ class _HomeState extends State<Home> {
           itemBuilder: (context, index) => this._buildRow(index)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          //print(UserInfo().token);
-          _readPreferences();
-          // postCourse(UserInfo().username, UserInfo().token).then(
-          //   (course) {
-          //     addCourse(course);
-          //     print("intento");
-          //   },
-          // ).catchError(
-          //   (error) {
-          //     if (error.toString() == 'Unauthorized') {
-          //       _unauthorizedProtocol();
-          //     }
-          //   },
-          // );
+          postCourse(UserInfo().username, UserInfo().token).then(
+            (course) {
+              addCourse(course);
+              print("intento");
+            },
+          ).catchError(
+            (error) {
+              if (error.toString() == 'Unauthorized') {
+                _unauthorizedProtocol();
+              }
+            },
+          );
         },
         child: Icon(Icons.add),
       ),
@@ -103,17 +95,17 @@ class _HomeState extends State<Home> {
     final prefs = await SharedPreferences.getInstance();
     String token = prefs.getString('token') ?? "none";
     // Check token
-    print(token);
-    String name = prefs.getString('name');
-    String username = prefs.getString('username');
-    String email = prefs.getString('email');
-    return UserInfo(
-      token: token,
-      username: username,
-      name: name,
-      email: email,
-    );
-    //futureList = fetchCourses(username, token);
+    Provider.of<Auth>(context).checkToken(token)
+        // if it's valid, load data
+        .then((isValid) {
+      if (isValid) {
+        String name = prefs.getString('name');
+        String username = prefs.getString('username');
+        String email = prefs.getString('email');
+
+        futureList = fetchCourses(username, token);
+      } else {}
+    });
   }
 
   _removePreferences() async {
