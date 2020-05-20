fff6ede7c8fdc276a3f5705391e4be9c0c5757e3
cafloivedu <cafloivedu@outlook.com>
2020-05-20 01:55:38 -0500

    cambios

--- lib/models/students_card.dart
+++ lib/models/students_card.dart
@@ -13,7 +13,9 @@ class StudentsCard extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(top: 8.0),
       child: InkWell(
-        onTap: ,
+        onTap: (){
+          //your logic here
+        },
       child: Card(
         margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
         child: ListTile(
