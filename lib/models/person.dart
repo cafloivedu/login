6c42801d61f99c5c6f524d1b2c50dedac472ba93
cafloivedu <cafloivedu@outlook.com>
2020-05-20 00:27:27 -0500

    cambios

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -15,5 +15,32 @@ class PersonInfo {
       email: json['email'],
     );
   }
+}
 
+class Person {
+  final int courseId;
+  final String name;
+  final String username;
+  final String birthday;
+  final String email;
+  final String city;
+  final String country;
+  final String phone;
+
+  const Person({this.courseId, this.name, this.username, this.birthday,
+      this.email, this.city, this.country, this.phone});
+
+  factory Person.fromJson(Map<String, dynamic> json) {
+    final personJson = json['person'];
+    return Person(
+      courseId: json['course_id'],
+      name: json['name'],
+      username: json['username'],
+      birthday: json['birthday'],
+      email: json['email'],
+      city: json['city'],
+      country: json['country'],
+      phone: json['phone'],
+    );
+  }
 }
