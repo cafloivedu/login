1e373353d2f53d0a46b99826e782e3c195e15c49
Hernan Parra <hernandre1122@gmail.com>
2020-05-20 00:44:01 -0500

    create details info

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 class UserDetails extends StatefulWidget {
+
+  final int courseId;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  final String city;
+  final String country;
+  final String birth;
+
+  const UserDetails({this.courseId,this.name,this.username,this.email,this.phone,this.city,this.country,this.birth});
+
   @override
   _UserDetailsState createState() => _UserDetailsState();
 }
@@ -8,8 +20,24 @@ class UserDetails extends StatefulWidget {
 class _UserDetailsState extends State<UserDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 8.0),
+        child: Card(
+          margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 25.0,
+              backgroundColor: Colors.brown,
+            ),
+            title: Text(''),
+            subtitle: Text('Last Name: '),
+          ),
+        ),
+      ),
     );
   }
 }
