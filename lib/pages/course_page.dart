12b7e4e2527bdb89538889b3d62051d0ce3ea487
Hernan Parra <hernandre1122@gmail.com>
2020-05-19 23:32:46 -0500

    update students list

--- lib/pages/course_page.dart
+++ lib/pages/course_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login/actions/courses.dart';
 import 'package:login/models/course.dart';
+import 'package:login/models/students_card.dart';
 
 class CourseView extends StatefulWidget {
   final String pageName;
@@ -34,9 +35,17 @@ class _CourseViewState extends State<CourseView> {
       ),
       body: new ListView(
         children:<Widget>[ 
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Title(color: Colors.blueAccent, 
+          child: Center(
+            child: Text('Profesores',
+              style: new TextStyle(fontSize: 25),
+            ),
+          ), ),
+        ),
         new Card(
           margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
-
           child: InkWell(
             //onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => CourseView(pageName: name, username: username, token: token, courseId: courseId,))),
             child: ListTile(
@@ -44,13 +53,36 @@ class _CourseViewState extends State<CourseView> {
                 radius: 25.0,
                 backgroundColor: Colors.brown,
               ),
-              title: Text("name"),
-              subtitle: Text('Profesor: , Estudiantes: '),
+              title: Text(course.professor.name),
+              subtitle: Text('Email: ${course.professor.email}'),
             ),
           ), //new Text(course.professor.name),
         ),
-        ],
+        Padding(
+          padding: EdgeInsets.all(20.0),
+            child: Title(
+            color: Colors.black,
+            child: Text(
+              'Estudiantes',
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                fontSize: 25
+              ),
+            ),
+          ),
+        ),
+      ],
       ),
+      ListView.builder(
+        itemCount: course.students.length,
+        itemBuilder: (context, index) => this._buildRow(index),
+        ),
     );
   }
+
+  _buildRow(int index) {
+    return StudentsCard(name: course.students[index].name);
+  }
+
 }
+
