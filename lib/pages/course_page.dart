f3e98e3e9a0c82e1aec66a9c93052b1f6a55f11b
cafloivedu <cafloivedu@outlook.com>
2020-05-19 22:46:07 -0500

    cambios

--- lib/pages/course_page.dart
+++ lib/pages/course_page.dart
@@ -3,13 +3,10 @@ import 'package:login/actions/courses.dart';
 import 'package:login/models/course.dart';
 
 class CourseView extends StatefulWidget {
-
   final String pageName;
   final String token;
   final int courseId;
   final String username;
-
-
 
   const CourseView({this.pageName, this.token, this.courseId, this.username});
   @override
@@ -17,16 +14,15 @@ class CourseView extends StatefulWidget {
 }
 
 class _CourseViewState extends State<CourseView> {
-
   Course course;
-  
+
   @override
   void initState() {
     fetchCourse(widget.username, widget.courseId, widget.token).then((value) {
       setState(() {
         course = value;
       });
-    } );
+    });
     super.initState();
   }
 
@@ -36,8 +32,23 @@ class _CourseViewState extends State<CourseView> {
       appBar: AppBar(
         title: Text(course.name),
       ),
-      body: new Center(
-        child: new Text(course.professor.name),
+      body: new ListView(
+        children<Widget>[: new Card(
+          margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+
+          child: InkWell(
+            //onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => CourseView(pageName: name, username: username, token: token, courseId: courseId,))),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 25.0,
+                backgroundColor: Colors.brown,
+              ),
+              title: Text("name"),
+              subtitle: Text('Profesor: , Estudiantes: '),
+            ),
+          ), //new Text(course.professor.name),
+        ),
+        ]
       ),
     );
   }
